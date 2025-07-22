a8bd0a90a900ed8933caababe5ce2f77cb2b6294
Adam Anwar <mr.adam.anwar@gmail.com>
2025-07-22 21:13:19 +0800

    fixing test.sh and main.cpp

--- main.cpp
+++ main.cpp
@@ -28,8 +28,47 @@ int main() {
         player = Player(name);
     }
 
-    bool playing = true;
+    #ifdef TEST_MODE
+        Enemy enemy = Enemy::generateRandomEnemy(player.getLevel());
 
+        cout << "\n--- Enemy Appears! ---\n";
+        enemy.displayStats();
+        cout << "\n=== BATTLE START ===\n";
+
+        while (player.isAlive() && enemy.isAlive()) {
+            cout << "\nYour Turn:\n";
+            cout << "1. Attack\n2. View Stats\nChoose: ";
+            int choice;
+            cin >> choice;
+
+            if (choice == 1) {
+                player.attack(enemy);
+            } else {
+                player.displayStats();
+                continue;
+            }
+
+            if (enemy.isAlive()) {
+                cout << "\nEnemy's Turn:\n";
+                enemy.attack(player);
+            }
+        }
+
+        if (player.isAlive()) {
+            cout << "\nYou defeated the " << enemy.getName() << "!\n";
+            int earnedXP = enemy.getLevel() * 50;
+            player.addXP(earnedXP);
+            player.saveToFile("save.txt");
+        } else {
+            cout << "\nYou were defeated by the " << enemy.getName() << "...\n";
+        }
+
+        cout << "\n=== GAME OVER ===\n";
+        cout << "Thanks for playing Codebound!\n";
+        return 0;
+    #endif
+
+    bool playing = true;
     while (playing) {
         Enemy enemy = Enemy::generateRandomEnemy(player.getLevel());
 
