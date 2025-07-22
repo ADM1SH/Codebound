de3dca6417623e13550b9aab9d9189e0b3dd0141
Adam Anwar <mr.adam.anwar@gmail.com>
2025-07-22 16:06:04 +0800

    coded player.cpp

---
+++ Player.cpp
@@ -0,0 +1,29 @@
+// Player.cpp
+#include "Player.h"
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+Player::Player(string name)
+    : Character(name, 1, 100, 15, 5), xp(0), gold(0) {}
+
+void Player::attack(Character& target) {
+    int dmg = rand() % 10 + atk;
+    cout << name << " attacks for " << dmg << " damage!\n";
+    target.takeDamage(dmg);
+}
+
+void Player::levelUp() {
+    level++;
+    maxHp += 20;
+    atk += 5;
+    def += 2;
+    hp = maxHp;
+    cout << name << " leveled up! Now level " << level << ".\n";
+}
+
+void Player::displayStats() const {
+    Character::displayStats();
+    cout << "XP: " << xp << " | Gold: " << gold << "\n";
+}
